e803cdf7a2aacb224e2b1ca69110f55db99d1c6a
Dominick Schroer <dominick@schroer.ca>
2024-02-04 16:14:32 -0700

    move to obj

--- examples/b_spline.rs
+++ examples/b_spline.rs
@@ -1,71 +1,38 @@
-use nalgebra::Vector3;
-use pixel_canvas::{Canvas, Color, Image, XY};
-use splinosaurus::control_points::ControlGrid;
-use splinosaurus::export::Triangulation;
-use splinosaurus::surfaces::{BSurface, Surface};
-use std::fs::File;
+use nalgebra::Vector2;
+use splinosaurus::control_points::ControlVec;
+use splinosaurus::splines::{BSpline, Spline};
+use svg::node::element::path::Data;
+use svg::node::element::Path;
+use svg::Document;
 
 fn main() {
-    let mut grid = ControlGrid::new(
+    let grid = ControlVec::new(
         2,
-        3,
         vec![
-            //
-            Vector3::new(100., 100., 100.),
-            Vector3::new(200., 100., 100.),
-            Vector3::new(300., 100., 100.),
-            //
-            Vector3::new(100., 200., 100.),
-            Vector3::new(200., 200., 300.),
-            Vector3::new(300., 200., 100.),
-            //
-            Vector3::new(100., 300., 100.),
-            Vector3::new(200., 300., 100.),
-            Vector3::new(300., 500., 100.),
+            Vector2::new(100., 100.),
+            Vector2::new(200., 300.),
+            Vector2::new(300., 100.),
+            Vector2::new(400., 200.),
         ],
     );
-    grid.set_u_wrapping(true);
+    let spline = BSpline::new(grid);
 
-    let surface = BSurface::new(grid);
-
-    let t = Triangulation::new(0.1, &surface);
-
-    let mut out = File::create("a.stl").unwrap();
-    stl_io::write_stl(
-        &mut out,
-        t.triangles_with_normals().map(|(t, n)| stl_io::Triangle {
-            normal: stl_io::Vector::new([n.x, n.y, n.z]),
-            vertices: t.map(|v| stl_io::Vertex::new([v.x, v.y, v.z])),
-        }),
-    )
-    .unwrap();
-
-    let canvas = Canvas::new(512, 512).title("Tile");
-
-    canvas.render_on_change(true).render(move |_, image| {
-        image.fill(Color::WHITE);
-        let mut drawer = Drawer { image };
-
-        for p in surface.quantize(0.1) {
-            drawer.point(p[0] as usize, p[1] as usize, 5, Color::rgb(255, 0, 0));
-        }
-
-        for point in surface.control_points() {
-            drawer.point(point.x as usize, point.y as usize, 5, Color::BLACK);
-        }
-    });
-}
-
-struct Drawer<'a> {
-    image: &'a mut Image,
-}
-
-impl<'a> Drawer<'a> {
-    pub fn point(&mut self, x: usize, y: usize, size: usize, color: Color) {
-        for i in 0..size {
-            for j in 0..size {
-                self.image[XY(x + i, y + j)] = color
-            }
+    let mut data = Data::new();
+    for (i, p) in spline.quantize(0.01).enumerate() {
+        if i == 0 {
+            data = data.move_to((p.x, p.y));
+        } else {
+            data = data.line_to((p.x, p.y));
         }
     }
+
+    let path = Path::new()
+        .set("fill", "none")
+        .set("stroke", "black")
+        .set("stroke-width", 3)
+        .set("d", data);
+
+    let document = Document::new().set("viewBox", (0, 0, 500, 500)).add(path);
+
+    svg::save("image.svg", &document).unwrap();
 }
---
+++ examples/b_surface.rs
@@ -0,0 +1,53 @@
+use nalgebra::Vector3;
+use obj::{Group, IndexTuple, ObjData, Object, SimplePolygon};
+use splinosaurus::control_points::ControlGrid;
+use splinosaurus::export::Triangulation;
+use splinosaurus::surfaces::BSurface;
+use std::fs::File;
+
+fn main() {
+    let mut grid = ControlGrid::new(
+        2,
+        3,
+        vec![
+            //
+            Vector3::new(100., 100., 100.),
+            Vector3::new(200., 100., 100.),
+            Vector3::new(300., 100., 100.),
+            //
+            Vector3::new(100., 200., 100.),
+            Vector3::new(200., 200., 300.),
+            Vector3::new(300., 200., 100.),
+            //
+            Vector3::new(100., 300., 100.),
+            Vector3::new(200., 300., 100.),
+            Vector3::new(300., 500., 100.),
+        ],
+    );
+    grid.set_u_wrapping(true);
+
+    let surface = BSurface::new(grid);
+
+    let t = Triangulation::new(0.1, &surface);
+
+    let mut data = ObjData::default();
+    data.position = Vec::from_iter(t.points().iter().map(|p| [p.x, p.y, p.z]));
+    data.normal = Vec::from_iter(t.normals().iter().map(|p| [p.x, p.y, p.z]));
+    data.objects.push(Object {
+        name: "surface".to_string(),
+        groups: vec![Group {
+            name: "mesh".to_string(),
+            index: 0,
+            material: None,
+            polys: Vec::from_iter(t.indexed_triangles().iter().map(|t| {
+                SimplePolygon(vec![
+                    IndexTuple(t[0], None, None),
+                    IndexTuple(t[1], None, None),
+                    IndexTuple(t[2], None, None),
+                ])
+            })),
+        }],
+    });
+
+    data.save("a.obj").unwrap();
+}
